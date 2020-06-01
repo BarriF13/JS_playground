6279f385e5456d5f0d1da8deb4886c12ca5147d9
Barri <info@bdesignstudio.co.uk>
2020-06-01 18:25:21 +0100

    var , let , const differences

--- app.js
+++ app.js
@@ -19,3 +19,47 @@
 
 // var, const , let
 
+// var name = 'witch Wizard';
+// console.log(name);
+// name = 'Harry Potter';
+// console.log(name);
+
+// Initializing variable
+
+// var sayMyName; //used for conditional a lot
+// console.log(sayMyName);
+// sayMyName = 'Madonna';
+// console.log(sayMyName);
+
+//let
+
+
+// let name = 'witch Wizard';
+// console.log(name);
+// name = 'Harry Potter';
+// console.log(name);
+
+//Const -- can not assigned after it has been set up
+
+// const name = 'Witch Wizard';
+// console.log(name);
+// also cant not assign it if we don't initialize it first
+// const sayMyName;
+// sayMyName = 'madonna';
+// console.log(sayMyName); //missing initializer error
+
+// const magic = {
+//   name:'potions',
+//   time : 'two days spell',
+// }
+
+// magic.name = 'voodoo';
+// magic.time = 'a month';
+// console.log(magic);
+// in const we can change variables inside name and time - but can't change the const itself ---magic
+
+const num = [1,22,44,23];
+console.log(num);
+// num = [34,555]; // can't change it can be added 
+num.push(34 ,555);
+console.log(num);
