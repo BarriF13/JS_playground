bd2edd4fd855ef3b52516651fd2785729fc2acc8
Barri <info@bdesignstudio.co.uk>
2020-06-01 18:04:23 +0100

    consol options

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-console.log('hala lalay lalay');
-var greeting = "Hello";
-console.log(greeting +' Barri');
-console.log([1,3,55]);
-console.log({x:1, y:2});
-console.table({x:1, y:2})
+// console.log('hello it is me');
+// var greeting = "Hello";
+// console.log(greeting +' Barri');
+// console.log([1,3,55]);
+// console.log({x:1, y:2});
+// console.table({x:1, y:2})
 
-console.error('This is an error');
-// console.clear();
-console.warn('Beware of zombies');
-console.time('Hello');
-  console.log('Hello it is me');
-  console.log('Hello it is me');
-  console.log('Hello it is me');
-  console.log('Hello it is me');
-  console.log('Hello it is me');
-  console.log('Hello it is me');
-console.timeEnd('Hello');
+// console.error('This is an error');
+// // console.clear();
+// console.warn('Beware of zombies');
+// console.time('Hello');
+//   console.log('Hello it is me');
+//   console.log('Hello it is me');
+//   console.log('Hello it is me');
+//   console.log('Hello it is me');
+//   console.log('Hello it is me');
+//   console.log('Hello it is me');
+// console.timeEnd('Hello');
+
+// var, const , let
+
