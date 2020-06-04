244cfb0bed857c4548cd520c34d162b2a9536592
Barri <info@bdesignstudio.co.uk>
2020-06-04 19:23:04 +0100

    conditional

---
+++ app5.js
@@ -0,0 +1,33 @@
+// if(something is true) {
+//   do something
+// } else {
+//   do something else
+// }
+
+const id = 100;
+//Equal to 
+if (id ==101) {
+  console.log ('TADA ')
+} else {
+  console.log('Noway')
+}
+//Not Equal to
+
+if(id != 101 ){
+  console.log ('TADA ')
+} else {
+  console.log('No way')
+}
+
+//Equal to Value & Type 
+if (id ===100) {
+  console.log (' Smashing  ')
+} else {
+  console.log('Noway')
+}
+//Equal to Value & Type 
+if (id !==100) {
+  console.log (' Smashing  ')
+} else {
+  console.log('Noway')
+}
