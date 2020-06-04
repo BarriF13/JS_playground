1acf1e3c95793effd28ca1a0729e8526440b6e13
Barri <info@bdesignstudio.co.uk>
2020-06-04 12:45:04 +0100

    Arrays

---
+++ app3.js
@@ -0,0 +1,18 @@
+// creating array
+
+const num = [45, 66,23, 44 ,36,4]; // one way of making an array
+
+const num2 = new Array(22 ,67 ,88,1,21,13);
+
+const cars = ['Benz', ' lamborghini', 'ferrari', 'land rover', 'BMW' ];
+const mixed = ['Barri', 23, 'apple', true , null,]; //every type 
+
+let value;
+
+
+//
+
+
+
+console.log(num);
+console.log(value);
