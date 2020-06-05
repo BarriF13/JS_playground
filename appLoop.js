12a5f6d3fde9ac8167afdf4ef99debf12b128610
Barri <info@bdesignstudio.co.uk>
2020-06-05 10:12:05 +0100

    While loop

--- appLoop.js
+++ appLoop.js
@@ -1,22 +1,39 @@
 // FOR LOOP
 
-for(let i = 0; i < 10; i++){
-  console.log(i)
-}
-for(let i = 0; i < 10; i++){
-  console.log('number: '+ i)
-}
-// with conditional
-for(let i = 0; i < 10; i++){
-  if(i === 7 ){
-  console.log('number 7 is fab ');
-  continue;
-  }
-  if(i === 9){
-    console.log('that\' it I ma bored of loops')
-    break;
-  }
+// for(let i = 0; i < 10; i++){
+//   console.log(i)
+// }
+// for(let i = 0; i < 10; i++){
+//   console.log('number: '+ i)
+// }
+// // with conditional
+// for(let i = 0; i < 10; i++){
+//   if(i === 7 ){
+//   console.log('number 7 is fab ');
+//   continue;
+//   }
+//   if(i === 9){
+//     console.log('that\' it I ma bored of loops')
+//     break;
+//   }
 
-  console.log('Number ' +i)
-}
+//   console.log('Number ' +i)
+// }
+
+//------------------ WHILE LOOP
+
+// let i = 0 ;
+// while(i<13 ){
+//   console.log('number: '+ i);
+//   i++
+// }
+
+// DO WHILE
+// let i = 0;
+
+// do {
+//   console.log('number: '+ i);
+//   i++;
+// }
+// while(i < 10);
 
