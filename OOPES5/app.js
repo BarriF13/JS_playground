fed95f80ee812f1947833a4447bffc7f573b14fc
Barri <info@bdesignstudio.co.uk>
2020-06-07 19:59:23 +0100

    Build-in constructor

--- OOPES5/app.js
+++ OOPES5/app.js
@@ -1,24 +1,56 @@
-// Person constructor
+// Build in constructor
 
-function Person(name, dob){
-  this.name = name;
-  // this.age = age;
-  this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear()- 1970);
-  }
-  
+// not use very often
+// String
+const name1 = 'Zaghart'; // value
+const name2 = new String('Zaghart'); // Object
+
+//name2.foo = 'bar';
+
+console.log(name1);
+console.log(name2);
+
+if(name1 === 'Zaghart'){
+  console.log('yes');
+} else {
+  console.log(' NO ')
 }
 
-// console.log(this); // When this is using in global scope it is going to be pointed at window object
-// this.alert();
+// Number 
+const num1 = 5; // number
+const num2 = new Number(5); // object
+console.log(typeof num2); 
 
-// const barri = new Person('Barri', 22);
-// const madonna = new Person('Madonna', 100);
+// Boolean
+const bool1 = true; // value
+const bool2 = new Boolean(true); // object 
 
-// console.log(barri.name);
+console.log( typeof bool2);
 
-const barri = new Person('baba', '5-11-1974');
-console.log(barri.calculateAge());
+// Function 
+
+const getSum1 = function(x , y ) {
+  return x + y;
+}
+console.log(getSum1(3 , 4));
+
+const getSum2 = new Function('x', 'y', ' return x + y');
+console.log(getSum2(4 , 4));
+
+// Object 
+const jo = {name: 'Jo'};
+const jo1 = new Object( {name: 'Jo'});
+console.log(jo);
+console.log(jo1);
+
+// Array 
+const arr1 = [1,2];
+const arr2 = new Array(1,2);
+console.log(arr1);
+
+// Regular Expression 
+
+const re1 = /\w+/; 
+const re2 =  new RegExp('\w+');
+console.log(re1);
+console.log(re2);
