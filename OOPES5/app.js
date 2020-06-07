d2df0ba3e2f67f778f446e2f78003c97c3028e9c
Barri <info@bdesignstudio.co.uk>
2020-06-07 19:26:55 +0100

    Constroctor and DOB function

--- OOPES5/app.js
+++ OOPES5/app.js
@@ -1,12 +1,24 @@
 // Person constructor
 
-function Person(name){
+function Person(name, dob){
   this.name = name;
+  // this.age = age;
+  this.birthday = new Date(dob);
+  this.calculateAge = function(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear()- 1970);
+  }
   
 }
 
 // console.log(this); // When this is using in global scope it is going to be pointed at window object
 // this.alert();
 
-const barri = new Person('Barri');
-const madonna = new Person('Madonna');
+// const barri = new Person('Barri', 22);
+// const madonna = new Person('Madonna', 100);
+
+// console.log(barri.name);
+
+const barri = new Person('baba', '5-11-1974');
+console.log(barri.calculateAge());
