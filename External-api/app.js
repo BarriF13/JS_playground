5956a9c142688209dd0f6dbc8794cf02c398371b
Barri <info@bdesignstudio.co.uk>
2020-06-09 20:15:32 +0100

    load api on dom

--- External-api/app.js
+++ External-api/app.js
@@ -11,7 +11,19 @@ xhr.open('GET' ,`http://api.icndb.com/jokes/random/${number}`, true)
 xhr.onload= function(){
   if(this.status === 200){
    const response = JSON.parse(this.responseText);
-    console.log(response)
+
+    let output ='';
+    if(response.type === 'success'){
+      // depending of api we need to loop into it
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`;
+
+
+        });
+    } else {
+      output += '<li>Something went wrong</li>';
+    }
+    document.querySelector('.jokes').innerHTML = output;
   }
 }
 
