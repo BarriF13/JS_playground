8a0c5254dea8bcb819ed949555681162cf470bd2
Barri <info@bdesignstudio.co.uk>
2020-06-10 15:49:14 +0100

    fetch external API

--- Ajax/Fetch-api/app.js
+++ Ajax/Fetch-api/app.js
@@ -2,7 +2,7 @@
 
 document.getElementById('button1').addEventListener('click', getText);
 document.getElementById('button2').addEventListener('click', getJson);
-
+document.getElementById('button3').addEventListener('click', externalApi);
 function getText() {
   // fetch return promises-- res = response
   fetch('my-text.txt')
@@ -31,6 +31,27 @@ function getJson() {
       let output = '';
       data.forEach(function(post){
         output += `<li>${post.title}</li>`
+      });
+
+      document.getElementById('output').innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+}
+
+// External API
+function externalApi() {
+  // fetch return promises-- res = response
+  fetch('https://api.github.com/users')
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      let output = '';
+      data.forEach(function(user){
+        output += `<li>${user.login}</li>`
       });
 
       document.getElementById('output').innerHTML = output;
