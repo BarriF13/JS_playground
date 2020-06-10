99b2d8e9632bd91211a6574140654e38fa0ff7be
Barri <info@bdesignstudio.co.uk>
2020-06-10 13:41:58 +0100

    Create Promise ES6

--- call-back/app.js
+++ call-back/app.js
@@ -3,7 +3,7 @@ const posts = [
 {title: 'Post two', body: 'this is post  two'}
 
 ];
-
+//------------------------------------
 // function createPost(post){
 //   setTimeout(function() {
 //     posts.push(post);
@@ -23,11 +23,45 @@ const posts = [
 // createPost({title: 'Post Three', body: 'This is post three'});
 // getPosts();
 
-function createPost(post, callback){
+//----------------------------------------
+// function createPost(post, callback){
+//   setTimeout(function() {
+//     posts.push(post);
+//     callback();
+//   }, 2000);
+// }
+
+// function getPosts(){
+//   setTimeout(function(){
+//     let output = '';
+//     posts.forEach(function(post){
+//       output += `<li>${post.title}</li>`;
+//     });
+//     document.body.innerHTML = output;
+//   }, 1000);
+// }
+
+// createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+
+
+//----------------------------
+// Using promises --alternative to call back and used in ES6
+function createPost(post){
+//create a promise
+return new Promise(function(resolve, reject){
   setTimeout(function() {
     posts.push(post);
-    callback();
+
+    const error = true;
+    if(!error){
+      resolve();
+    }else{
+      reject('Error something went wrong');
+    }
+   
   }, 2000);
+});
+
 }
 
 function getPosts(){
@@ -40,4 +74,8 @@ function getPosts(){
   }, 1000);
 }
 
-createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+createPost({title: 'Post Three', body: 'This is post three'})
+.then(getPosts)
+.catch(function(error){
+  console.log(error);
+});
