e5f3e345e1253fa2ee3ef475ecb744c5800903a1
Barri <info@bdesignstudio.co.uk>
2020-06-10 17:20:23 +0100

    Using arrow function in app.js

--- Ajax/Fetch-api/app.js
+++ Ajax/Fetch-api/app.js
@@ -6,57 +6,52 @@ document.getElementById('button3').addEventListener('click', externalApi);
 function getText() {
   // fetch return promises-- res = response
   fetch('my-text.txt')
-    .then(function (res) {
-      return res.text();
-    })
-    .then(function (data) {
-      // console.log(data);
+    // .then(function (res) {
+    //   return res.text();
+    // })
+    // .then(function (data) {
+    //   // console.log(data);
 
-      document.getElementById('output').innerText = data;
-    })
-    .catch(function (err) {
-      console.log(err);
-    })
+    //   document.getElementById('output').innerText = data;
+    // })
+    //  .catch(function (err) {
+    //   console.log(err);
+    // })
+    .then(res => res.text())
+    .then(data => document.getElementById('output').innerText = data)
+    .catch(err => console.log(err));
 }
 
 // local Json data
 function getJson() {
   // fetch return promises-- res = response
   fetch('my-post.JSON')
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
       let output = '';
-      data.forEach(function(post){
-        output += `<li>${post.title}</li>`
+      data.forEach(function (post) {
+        output += `<li>${post.title}</li>`;
       });
-
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function (err) {
-      console.log(err);
-    })
+    .catch(err => console.log(err));
 }
 
 // External API
 function externalApi() {
   // fetch return promises-- res = response
   fetch('https://api.github.com/users')
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
       let output = '';
-      data.forEach(function(user){
+      data.forEach(function (user) {
         output += `<li>${user.login}</li>`
       });
 
       document.getElementById('output').innerHTML = output;
     })
-    .catch(function (err) {
-      console.log(err);
-    })
+    .catch(err => console.log(err));
 }
+
