50bd27b9184e343f3f1e206acf7970a3d2094616
Barri <info@bdesignstudio.co.uk>
2020-06-10 19:01:08 +0100

    Use asynce await of the fetch

--- Async-await/app.js
+++ Async-await/app.js
@@ -24,20 +24,34 @@
 // .then(res => console.log(res));
 
 // Now with error 
-async function myFunc(){
-  const promise = new Promise((resolve, reject)=> {
-    setTimeout(()=> resolve('Hello'), 1000);
-  });
+// async function myFunc(){
+//   const promise = new Promise((resolve, reject)=> {
+//     setTimeout(()=> resolve('Hello'), 1000);
+//   });
 
-  const error = true;
-  if( error ){
-    await Promise.reject(new Error('something wen wrong'));
-  }else {
-    const res = await promise;//wait until promise is resolved 
-    return res;
-  }
+//   const error = true;
+//   if( error ){
+//     await Promise.reject(new Error('something wen wrong'));
+//   }else {
+//     const res = await promise;//wait until promise is resolved 
+//     return res;
+//   }
 
+// }
+// myFunc()
+// .then(res => console.log(res))
+// .catch(err => console.log(err))
+
+// Using   async await with fetch
+async function getUsers(){
+//await response of the fetch call
+
+  const response = await fetch
+  ('https://jsonplaceholder.typicode.com/users');
+  // Only proceed once its resolved
+  const data = await response.json();
+  // Only proceed once second promise is resolved
+  return data;
 }
-myFunc()
-.then(res => console.log(res))
-.catch(err => console.log(err))
+
+getUsers().then(users => console.log(users));
