86c462c14e366c8a3ee427ab8b974cc7cdf7e77f
Barri <info@bdesignstudio.co.uk>
2020-06-12 20:17:19 +0100

    REVEALING MODULE PATTTERN

--- Pattern/app.js
+++ Pattern/app.js
@@ -11,20 +11,45 @@
 //   }
 // })();
 
-const UICtrl = (function(){
-// Private var
-let text = 'Hello Cyrus';
-// private function expression
-const changeText = function(){
-  const element = document.querySelector('h1');
-  element.textContent = text;
-}
-  return{
-   callChangeText: function() {
-     changeText();
-     console.log(text);
+// const UICtrl = (function(){
+// // Private var
+// let text = 'Hello Cyrus';
+// // private function expression
+// const changeText = function(){
+//   const element = document.querySelector('h1');
+//   element.textContent = text;
+// }
+//   return{
+//    callChangeText: function() {
+//      changeText();
+//      console.log(text);
+//    }
+//   }
+// })();
+// UICtrl.callChangeText();
+// UICtrl.changeText(); // we can't access this because it is private
+
+// REVEALING MODULE PATTERN
+
+const itemCtrl = (function(){
+   let data =[];
+   
+   function add(item){
+    data.push(item);
+    console.log('Item Added...');
    }
-  }
+   function get(id){
+     return data.find(item =>{
+       return item.id === id;
+     });
+   }
+
+    return {
+      //object literal -- to revealing above method
+      add: add,
+      get: get
+    }
 })();
-UICtrl.callChangeText();
-UICtrl.changeText(); // we can't access this because it is private
+
+itemCtrl.add({id: 1, name: 'Bobo'});
+console.log(itemCtrl.get(1));
