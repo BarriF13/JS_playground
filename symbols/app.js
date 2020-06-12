e978ab3f098530946d8308d8faaf938eea3073cb
Barri <info@bdesignstudio.co.uk>
2020-06-12 20:33:56 +0100

    Singleton Pattern

--- symbols/app.js
+++ symbols/app.js
@@ -1,38 +1,28 @@
-// Create a symbol
+// Singleton pattern -- rarely use by teachers
 
-// const sym1 = Symbol();
-// const sym2 = Symbol('sym2');
+const Singleton = (function(){
 
+    let instance;
 
-// it is one and only 
-// console.log( Symbol('123') === Symbol('123'));
+    function createInstance() {
+      const object = new Object({name: 'Baba'});
+      return object;
+    }
 
-// can not convert to string -- we can wrap or . toSting it
-// console.log(`Hello ${sym1.toString()}`)
+      return {
+        getInstance: function(){
+          if(!instance){
+             instance = createInstance();
+          }
+          return instance;
+        }
 
-// Symbol used for = Unique object keys
-const KEY1 = Symbol();
-const KEY2 = Symbol('sym2'); //--'sym2' is the id for symbol
+    }
+})();
 
-const myObj = {};
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
 
-// to give myObj a key as property we use []
+console.log(instanceA === instanceB); // gives us true
 
-myObj[KEY1] = 'Prop1';
-myObj[KEY2] = 'Prop2';
-
-myObj.key3 = 'Prop3';
-myObj.key4 = 'Prop4';
-
-// console.log(myObj[KEY1]);
-
-// Symbols are not enumerable in for ... in 
-
-// for(let i in myObj) {
-//   console.log(`${i}: ${myObj[i]}`); // it only gives 2 regular properties not symbols
-// }
-
-// Symbols also are ignored if we use JSON.stringfy (they take js object literal and change it to json string)
-
-console.log(JSON.stringify({key : 'Prop'})); // gives json string
-console.log(JSON.stringify({[Symbol('sym1')] : 'Prop'})); // gives empty object 
+// console.log(instanceA);
